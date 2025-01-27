ac7a08e0f38b72f562721480f8168cca4f1c2c94
Aden Fernandes <adenferns999@gmail.com>
2025-01-28 02:41:19 +0530

    init: Added the frontend files
    - Added the pages and routes
    - Added the components and the loaders
    - Added the updated configs for tailwinds and acernity UI
    - Added Tailwind CSS to the project.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+
+import Header from "./components/ui/Header";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignupPage";
+import TransactionPage from "./pages/TransactionPage";
+import NotFound from "./pages/NotFound";
+
+function App() {
+	const authUser = true;
+	return (
+		<>
+			{authUser && <Header />}
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/transaction/:id' element={<TransactionPage />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</>
+	);
+}
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React  from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
+import GridBackground from './components/ui/GridBackground.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    		<BrowserRouter>
+			<GridBackground>
+				<App />
+			</GridBackground>
+		</BrowserRouter>
+
+  </React.StrictMode>,
+)
